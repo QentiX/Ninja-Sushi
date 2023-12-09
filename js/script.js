2c10dc6cb578ef16a9ccda2766e9450c1314e4ee
QentiX <programmisteconomist21@gmail.com>
2023-12-09 23:16:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.header__menu')
+const menuBtn = document.querySelector('.header__burger')
+const body = document.body
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    body.classList.toggle('lock')
+  })
+
+  menu.querySelectorAll('.menu__link').forEach(link =>{
+    link.addEventListener('click', () => {
+      menu.classList.remove('active')
+      menuBtn.classList.remove('active')
+      body.classList.remove('lock')
+    })
+  })
+}
